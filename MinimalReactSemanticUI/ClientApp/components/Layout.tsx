9fecdb7de5d9e50eb62ca5161a1c0bcf98e7e2af
UltimaBeaR <thebear@yandex.ru>
2017-11-26 19:32:44 +0300

    Initial

---
+++ MinimalReactSemanticUI/ClientApp/components/Layout.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from 'react';
+import { Container } from 'semantic-ui-react';
+
+export interface LayoutProps {
+    children?: React.ReactNode;
+}
+
+export class Layout extends React.Component<LayoutProps, {}> {
+    public render() {
+        return (
+            <Container>
+                { this.props.children }
+            </Container>
+        );
+    }
+}
