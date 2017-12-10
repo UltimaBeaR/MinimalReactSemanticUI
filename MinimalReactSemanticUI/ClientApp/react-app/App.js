1a57591692c7bfe9c8b8fd506093526529656fc4
UltimaBeaR <thebear@yandex.ru>
2017-12-11 01:33:38 +0300

    Eslint config added, linting errors fixed

--- MinimalReactSemanticUI/ClientApp/react-app/App.js
+++ MinimalReactSemanticUI/ClientApp/react-app/App.js
@@ -3,7 +3,7 @@
 // global css rules (everything like *.global.scss or *.global.css is treated as global stylesheet. See webpack config)
 import './App.global.scss';
 // local css rules for current component (any other scss or css file is considered as local, css modules are used in this case)
-import classes from './App.scss'
+import classes from './App.scss';
 
 // import <Label> component from semantic-ui react package. Docs are here: https://react.semantic-ui.com
 import { Label } from 'semantic-ui-react';
@@ -13,9 +13,9 @@ class App extends Component {
         return (
             // Use semantic-ui <Label> react component for styling label background borders and so on,
             // and global / local classes for text color styling inside of <Label> component
-            <div style={ { margin: "10px" } }>
+            <div style={ { margin: '10px' } }>
                 <Label>
-                    <span className="colored-text">Hello</span> <span className={ classes["colored-text"] }>world</span>!
+                    <span className="colored-text">Hello</span> <span className={ classes['colored-text'] }>world</span>!
                 </Label>
             </div>
         );
